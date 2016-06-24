01477f9d8ef82c51b8e3422899df1dba5fc7e549
Reza Shirazian <reza.shirazian@gmail.com>
2016-06-23 22:20:14 -0700

    Initial Commit

---
+++ BinarySearchTree/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  BinarySearchTree
+//
+//  Created by Reza Shirazian on 2016-06-23.
+//  Copyright © 2016 Reza Shirazian. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
