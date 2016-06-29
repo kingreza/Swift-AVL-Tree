fc5714fe79b5689670d1efefa8d5172d44db6aa5
Reza Shirazian <reza.shirazian@gmail.com>
2016-06-28 21:38:47 -0700

    Main clean up

--- BinarySearchTree/main.swift
+++ BinarySearchTree/main.swift
@@ -14,7 +14,6 @@ print("Hello, World!")
 
 var tree = BinarySearchTree<Int>()
 
-
 tree.insert(7)
 tree.insert(3)
 tree.insert(4)
@@ -23,11 +22,6 @@ tree.insert(2)
 tree.insert(1)
 
 var node = tree.findNode(7)
-
-var next = tree.predecessor(node!)
-print(next!.value)
-
-print("Hello World! Again!")
 
 for index in 0..<tree.count {
   print(tree[index])
@@ -39,3 +33,7 @@ var reverse = tree.reverse()
 for value in reverse {
   print(value)
 }
+
+var newTree: BinarySearchTree = ["hello", "world", "this", "is", "a", "new", "day"]
+
+_ = newTree.map {print($0)}
